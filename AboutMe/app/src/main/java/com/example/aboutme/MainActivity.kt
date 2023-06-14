61f33b337f87b45d317d08a4d9b51c0e06a9a11b
notAxion <mr.bhakat456@gmail.com>
2023-06-14 20:51:22 +0530

    wired the frontend to hide the EditView and Button and show only a TextView of what was entered in place of it

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -11,5 +14,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.doneButton.setOnClickListener{
+            addNickName()
+            it.visibility = View.GONE
+        }
+    }
+
+    private fun addNickName() {
+        val editView = binding.nicknameEdit
+        val nicknameText = binding.nicknameText
+
+        nicknameText.text = editView.text
+        editView.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide the keyboard
+        // no clue how it works yet
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
     }
 }
