2f7429d72261342a8feb41a825a9aa14027a2029
notAxion <mr.bhakat456@gmail.com>
2023-06-17 17:23:42 +0530

    added the 2nd fragment on play

--- trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -22,6 +23,9 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 
