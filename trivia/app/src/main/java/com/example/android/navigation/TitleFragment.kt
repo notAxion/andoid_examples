8e6d8e10f1e2f2becb016f068d4114558547f95e
notAxion <mr.bhakat456@gmail.com>
2023-06-17 17:24:20 +0530

    another way to get the nav controller

--- trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -23,8 +23,8 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
-        binding.playButton.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        binding.playButton.setOnClickListener {view:View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
