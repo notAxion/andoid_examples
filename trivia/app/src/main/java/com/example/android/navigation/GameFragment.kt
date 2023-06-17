b4ef392cb160646a07521e6d829f024f0d58c2ac
notAxion <mr.bhakat456@gmail.com>
2023-06-18 01:28:42 +0530

    used safeargs to bundle arguements from one fragment to another

--- trivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 
@@ -98,11 +99,16 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
-                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
+                        val gameWonDirections: NavDirections = GameFragmentDirections
+                            .actionGameFragmentToGameWonFragment(numQuestions,questionIndex)
+
+                        view.findNavController().navigate(gameWonDirections)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
-                    view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment2)
+                    val gameOverDirections: NavDirections = GameFragmentDirections
+                        .actionGameFragmentToGameOverFragment2()
+                    view.findNavController().navigate(gameOverDirections)
                 }
             }
         }
--- trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -35,6 +36,12 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment2)
         }
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(
+            context,
+            "NumCorrect: ${args.numCorrect} NumQuestion ${args.numQuestions}",
+            Toast.LENGTH_LONG
+        ).show()
         return binding.root
     }
 }
