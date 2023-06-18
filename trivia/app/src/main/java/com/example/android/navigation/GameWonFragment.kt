ef9c6dbc0a7d3b8bca6237f99644700f72aeb4b3
notAxion <mr.bhakat456@gmail.com>
2023-06-18 13:41:19 +0530

    used the ShareCompat to set the intent
    its just another way of doing the intent no clue if its better or not at this point

--- trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ trivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -25,6 +25,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
@@ -56,12 +57,20 @@ class GameWonFragment : Fragment() {
 
     private fun getShareIntent(): Intent {
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-            .putExtra(Intent.EXTRA_TEXT,
-                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.setType("text/plain")
+//            .putExtra(Intent.EXTRA_TEXT,
+//                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+//            )
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(
+                getString(R.string.share_success_text,
+                    args.numCorrect,
+                    args.numQuestions
+                )
             )
-        return shareIntent
+            .setType("text/plain")
+            .intent
     }
 
     private fun shareSuccess() {
