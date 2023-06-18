f26a1537f69d5ba2c0a3ca83f7c1ff94a1788b1a
notAxion <mr.bhakat456@gmail.com>
2023-06-18 23:04:44 +0530

    some use cases of navigation listeners, stopping the drawer outside of title fragment

--- trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ trivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -37,6 +38,15 @@ class MainActivity : AppCompatActivity() {
 
         navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
         // this enables the menu item to use the id
         // for navigation
         NavigationUI.setupWithNavController(binding.navView, navController)
