98de8f0c3cc112791c4a75e01435b1ae60681af3
notAxion <mr.bhakat456@gmail.com>
2023-06-21 15:51:05 +0530

    implemented the ViewModel class for game fragment

--- guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,14 +22,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
+import timber.log.Timber
 
 /**
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -52,6 +55,9 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
+
+        Timber.i("called ViewModelProvider")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
---
+++ guess-it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class GameViewModel: ViewModel() {
+    init {
+        Timber.i("GameViewModel created!!")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("GameViewModel destroyed!!")
+    }
+}
