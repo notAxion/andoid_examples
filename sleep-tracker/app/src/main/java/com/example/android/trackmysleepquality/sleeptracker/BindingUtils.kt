9673ba46e45a299d3d252e88399f14ece747a960
notAxion <mr.bhakat456@gmail.com>
2023-06-26 17:57:23 +0530

    refactor to use BindingAdapter
    although in this case it wasn't necessary, because the property binds to every other view(same view that we use the extension for)
    A good way to do this kind of encapsulation would be to use a custom View
    shown in the comments

--- sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ sleep-tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -15,3 +15,52 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+// there is also a way to create a custom ImageView class
+// class SleepImageView() : ImageView() {
+//     fun setSleepImage(item: SleepNight) {
+//         // ...
+//     }
+// }
+// so that we don't have extra property on all of the ImageView
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(/* resId = */when (item.sleepQuality) {
+        0 -> R.drawable.ic_sleep_0
+        1 -> R.drawable.ic_sleep_1
+        2 -> R.drawable.ic_sleep_2
+        3 -> R.drawable.ic_sleep_3
+        4 -> R.drawable.ic_sleep_4
+        5 -> R.drawable.ic_sleep_5
+        else -> R.drawable.ic_sleep_active
+    })
+}
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDuratinFormatted(item: SleepNight?) {
+    item?.let {
+        text = convertDurationToFormatted(
+            startTimeMilli = item.startTimeMilli,
+            endTimeMilli = item.endTimeMilli,
+            res = context.resources
+        )
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight?) {
+    item?.let {
+        text = convertNumericQualityToString(
+                quality = item.sleepQuality,
+                res = context.resources
+            )
+    }
+}
