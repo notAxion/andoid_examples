b2eef3aa538f33835ebef405eed09e949bc1f166
notAxion <mr.bhakat456@gmail.com>
2023-07-10 20:14:27 +0530

    added a imageview to show status when there is no internet
    the status does not get shown while the app is active
    it only shows when the app has launched and the staus does not go away if the internet comes online while the app is active

--- mars-realestate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ mars-realestate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.marsrealestate
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
@@ -24,6 +25,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
 import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
 @BindingAdapter("listData")
@@ -42,5 +44,22 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                 .placeholder(R.drawable.loading_animation)
                 .error(R.drawable.ic_broken_image))
             .into(imgView)
+    }
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(imgView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING-> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR-> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE-> {
+            imgView.visibility = View.GONE
+        }
     }
 }
