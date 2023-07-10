d17f95f0ba721022502a8d5fe7b8ee0708633892
notAxion <mr.bhakat456@gmail.com>
2023-07-10 17:35:04 +0530

    showing the image for the first property of mars
    with glide

--- mars-realestate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ mars-realestate/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,3 +17,17 @@
 
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("imgUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let { url->
+        val imgUri = url.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
+}
